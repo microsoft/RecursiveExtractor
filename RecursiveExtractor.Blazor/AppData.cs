14b69d7ce1d58b0e83804010e484d0bc0478755f
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-08-04 15:21:20 -0700

    Initial commit

---
+++ RecursiveExtractor.Blazor/AppData.cs
@@ -0,0 +1,17 @@
+﻿using DiscUtils.Ntfs.Internals;
+using Microsoft.CST.RecursiveExtractor;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RecursiveExtractor.Blazor.Services
+{
+    public class AppData
+    {
+        public AppData()
+        {
+            
+        }
+
+        public Dictionary<string, FileEntry> GuidToFileEntryMap { get; set; } = new Dictionary<string, FileEntry>();
+    }
+}
