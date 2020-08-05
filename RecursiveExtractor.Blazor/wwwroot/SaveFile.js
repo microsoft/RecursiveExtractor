bab9ce578014137c7ee7f733f9461964d36293ed
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-08-05 08:15:18 -0700

    Adds download button

---
+++ RecursiveExtractor.Blazor/wwwroot/SaveFile.js
@@ -0,0 +1,8 @@
+﻿function FileSaveAs(filename, fileEncoding, fileContent) {
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = fileEncoding + encodeURIComponent(fileContent)
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
