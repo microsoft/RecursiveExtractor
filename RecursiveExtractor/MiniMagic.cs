2e55d14e025d292fcf404e85fc3437d80a08674f
Gabe Stocco <98900+gfs@users.noreply.github.com>
2020-08-16 07:06:23 -0700

    Update MiniMagic.cs

--- RecursiveExtractor/MiniMagic.cs
+++ RecursiveExtractor/MiniMagic.cs
@@ -92,7 +92,7 @@ namespace Microsoft.CST.RecursiveExtractor
                 }
                 if (buffer[0] == 0x52 && buffer[1] == 0x61 && buffer[2] == 0x72 && buffer[3] == 0x21 && buffer[4] == 0x1A && buffer[5] == 0x07 && buffer[6] == 0x01 && buffer[7] == 0x00)
                 {
-                    return ArchiveFileType.RAR5
+                    return ArchiveFileType.RAR5;
                 }
                 if (buffer[0] == 0x37 && buffer[1] == 0x7A && buffer[2] == 0xBC && buffer[3] == 0xAF && buffer[4] == 0x27 && buffer[5] == 0x1C)
                 {
