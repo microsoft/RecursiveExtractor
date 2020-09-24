6780b76a0db693445b0662db0971216baddc9152
Michael Scovetta <michael.scovetta@microsoft.com>
2020-09-24 12:29:14 -0700

    Minor tweaks to help text.

--- RecursiveExtractor.Cli/ExtractCommandOptions.cs
+++ RecursiveExtractor.Cli/ExtractCommandOptions.cs
@@ -3,31 +3,31 @@ using System.Collections.Generic;
 
 namespace Microsoft.CST.RecursiveExtractor.Cli
 {
-	[Verb("extract", true, HelpText = "Add file contents to the index.")]
+	[Verb("extract", true, HelpText = "Extract an archive recursively.")]
 	public class ExtractCommandOptions
 	{
-        [Option('i',"input",HelpText = "The name of the Archive to extract.", Required = true)]
+        [Option('i',"input", HelpText = "The name of the archive to extract.", Required = true)]
 		public string Input { get; set; } = string.Empty;
 
 		[Option('o', "output", HelpText = "The directory to extract to.", Default = ".")]
 		public string Output { get; set; } = string.Empty;
 
-        [Option('p', "passwords", Required = false, HelpText = "Comma separated list of passwords to use.", Separator = ',')]
+        [Option('p', "passwords", Required = false, HelpText = "Comma-separated list of passwords to use.", Separator = ',')]
         public IEnumerable<string>? Passwords { get; set; }
 
-        [Option('A', "allow-filters", Required = false, HelpText = "Comma separated list of regexes.  When set, files are only written to disc if they match one of these filters.", Separator = ',')]
+        [Option('A', "allow-filters", Required = false, HelpText = "Comma-separated list of regular expressions. When set, files are ONLY written to disk if they match one of these filters.", Separator = ',')]
         public IEnumerable<string>? AllowFilters { get; set; }
 
-        [Option('D', "deny-filters", Required = false, HelpText = "Comma separated list of regexes.  When set, files are not written to disc if they match one of these filters.", Separator = ',')]
+        [Option('D', "deny-filters", Required = false, HelpText = "Comma-separated list of regular expressions. When set, files are NOT written to disk if they match one of these filters.", Separator = ',')]
         public IEnumerable<string>? DenyFilters { get; set; }
 
-        [Option(HelpText = "Set logging to Verbose.")]
+        [Option(HelpText = "Set logging to 'verbose'.")]
 		public bool Verbose { get; set; }
 
-        [Option(HelpText = "Set logging to Debug.")]
+        [Option(HelpText = "Set logging to 'debug'.")]
         public bool Debug { get; set; }
 
-        [Option(HelpText = "Print names of all extracted files.")]
+        [Option(HelpText = "Output the names of all files extracted.")]
         public bool PrintNames { get; set; }
     }
 }
