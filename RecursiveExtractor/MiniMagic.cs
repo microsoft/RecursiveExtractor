9c45ff1418219f0e59048b374f1e2945f8edb016
Ibrahim Akgul <loginit@gmail.com>
2023-01-19 00:12:54 +0300

    Update MiniMagic.cs
    Add enum, and signature check

--- RecursiveExtractor/MiniMagic.cs
+++ RecursiveExtractor/MiniMagic.cs
@@ -232,6 +232,10 @@ namespace Microsoft.CST.RecursiveExtractor
                 {
                     return ArchiveFileType.ISO_9660;
                 }
+                else if (buffer[0] == 'B' && buffer[1] == 'E' && buffer[2] == 'A' && buffer[3] == '0' && buffer[4] == '1')
+                {
+                    return ArchiveFileType.UDF;
+                }
             }
 
             //https://www.microsoft.com/en-us/download/details.aspx?id=23850 - 'Hard Disk Footer Format'
