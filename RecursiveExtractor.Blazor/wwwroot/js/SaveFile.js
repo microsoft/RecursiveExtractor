23dd26e7626c871ddb3adccc62c42e3fbefd4e13
Gabe Stocco <98900+gfs@users.noreply.github.com>
2025-01-14 12:29:24 -0800

    Update Dependencies, Add Net 9.0 Target, Remove Deprecated Blazor Project (#163)
    * Remove Deprecated Blazor Interface
    
    * Update dependencies
    
    * Update pipeline net versions
    
    * Set DotNetVersion to 9.0.x

--- RecursiveExtractor.Blazor/wwwroot/js/SaveFile.js
+++
@@ -1,8 +0,0 @@
-﻿function FileSaveAs(filename, fileEncoding, fileContent) {
-    var link = document.createElement('a');
-    link.download = filename;
-    link.href = fileEncoding + encodeURIComponent(fileContent)
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
